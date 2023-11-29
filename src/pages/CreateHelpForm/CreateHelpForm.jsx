5b3176443defca9e79611119e18af96fc8ecc8cc
lino zimerman <linozimerman@gmail.com>
2023-11-29 19:27:30 +0100

    created jsx+css files

---
+++ src/pages/CreateHelpForm/CreateHelpForm.jsx
@@ -0,0 +1,11 @@
+import "./CreateHelpForm.css";
+
+function CreateHelpForm() {
+    return (
+        <div>
+            <h1>User Profile</h1>
+        </div>
+    );
+}
+
+export default CreateHelpForm;
---
+++ src/pages/TestimonialPage/TestimonialPage.jsx
@@ -0,0 +1,11 @@
+import "./TestimonialPage.css";
+
+function TestimonialPage() {
+    return (
+        <div>
+            <h1>Testimonial Page</h1>
+        </div>
+    );
+}
+
+export default TestimonialPage;
