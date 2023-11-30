5d3329b2fcf65f333080c038179503711005c561
claratesmon <claraperezmontes@gmail.com>
2023-11-30 12:46:07 +0100

    nav bar start

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import HomePage from "./pages/HomePage/HomePage";
 import ProfilePage from "./pages/ProfilePage/ProfilePage";
 import SignupPage from "./pages/SignupPage/SignupPage";
 import LoginPage from "./pages/LoginPage/LoginPage";
-
+import LandingPage from "./pages/LandingPage/LandingPage";
 import Navbar from "./components/Navbar/Navbar";
 import IsPrivate from "./components/IsPrivate/IsPrivate";
 import IsAnon from "./components/IsAnon/IsAnon";
@@ -16,7 +16,15 @@ function App() {
       <Navbar />
 
       <Routes>
-        <Route path="/" element={<HomePage />} />
+        <Route path="/" element={
+          <IsPrivate>
+             <HomePage /> 
+          </IsPrivate> 
+        
+        
+        } />
+
+
 
         <Route
           path="/profile"
--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -4,7 +4,7 @@ const LandingPage = () => {
 
     return (
         <>
-
+            <h1>Landing Page</h1>
         </>
     );
 }
