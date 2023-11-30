f59a2bc2b92a156d77b40de4f388161517a33a53
John Doe <johndoe@example.com>
2023-11-30 10:14:33 +0100

    simo front updated

--- src/pages/CreateFamily/CreateFamily.jsx
+++ src/pages/CreateFamily/CreateFamily.jsx
@@ -1,11 +1,20 @@
 import "./CreateFamily.css";
 function CreateFamily() {
+    function getRandomNumber() {
+        return Math.floor(Math.random() * (999999 - 100000 + 1)) + 100000;
+    }
+    let randomNum = getRandomNumber();
+ /* add and if to check if the number already exists in the database */
     return (
-        <form>
-            <label>Family name:</label>
-            <input type="text" name="name" />
-            <button type="submit">Create family</button>
-        </form>
+        <div>
+            <h2>Your family number is: {randomNum}</h2>
+            <p>Give this number to the others menmbers of the family so that they can join!</p>
+            <form>
+                <label>Family name:</label>
+                <input type="text" name="name" />
+                <button type="submit">Create family</button>
+            </form>
+        </div>
     )
 }
 export default CreateFamily;
--- src/pages/CreateTask/CreateTask.jsx
+++ src/pages/CreateTask/CreateTask.jsx
@@ -1,7 +1,54 @@
 import "./CreateTask.css";
-function CreateTask () {
+import { Link } from "react-router-dom";
+function CreateTask() {
     return (
-        <h1>TESTED</h1>
+        <form className="form-conteiner">
+            {/*falta and task icon, falta taskindone, falta assigned to, falta taskImgUploaded, falta task comments */}
+            <label>Description:</label>
+            <textarea id="content" name="description" required></textarea>
+            <br/>
+            <label for="dayMoment"><b>When? Choose a timing interval to finish the task</b></label>
+            <select id="timing-interval" name="timingInterval">
+                <option value="On Wake Up">On Wake Up</option>
+                <option value="Before Breakfast">Before Breakfast</option>
+                <option value="After Breakfast">After Breakfast</option>
+                <option value="Before Lunch">Before Lunch</option>
+                <option value="After Lunch">After Lunch</option>
+                <option value="In the afternoon">In the Afternoon</option>
+                <option value="Before Dinner">In the Afternoon</option>
+                <option value="After Dinner">In the Afternoon</option>
+                <option value="Before Sleep">In the Afternoon</option>
+            </select>
+            <br/>
+            <label for="dayMoment"><b>Choose a day of the week</b></label>
+            <select id="day-moment" name="dayMoment">
+                <option value="Everyday">Everyday</option>
+                <option value="Monday">Monday</option>
+                <option value="Tuesday">Tuesday</option>
+                <option value="Wednesday">Wednesday</option>
+                <option value="Thursday">Thursday</option>
+                <option value="Friday">Friday</option>
+                <option value="Saturday">Saturday</option>
+                <option value="Sunday">Sunday</option>
+            </select>
+            <br/>
+            <label for="dayMoment"><b>Choose a day of the week</b></label>
+            <select id="day-moment" name="dayMoment">
+                <option value="Everyday">Everyday</option>
+                <option value="Monday">Monday</option>
+                <option value="Tuesday">Tuesday</option>
+                <option value="Wednesday">Wednesday</option>
+                <option value="Thursday">Thursday</option>
+                <option value="Friday">Friday</option>
+                <option value="Saturday">Saturday</option>
+                <option value="Sunday">Sunday</option>
+            </select>
+            <br/>
+            <Link to="/">
+            <button type="submit">Create task</button>
+            </Link>
+        </form>
     )
 }
 export default CreateTask;
+
