0c3786cf8a1b15549d71aa146fc464ea15326ba8
Rafael Gerotto <rafael.gerotto@hotmail.com>
2023-11-30 17:37:03 +0100

    creating path for create task

--- src/App.jsx
+++ src/App.jsx
@@ -24,7 +24,7 @@ function App() {
           path="/"
           element={
             <IsPrivate>
-              <HomePage />
+              <HomePage/>
             </IsPrivate>
           }
         />
