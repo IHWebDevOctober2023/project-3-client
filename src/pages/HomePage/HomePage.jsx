63a2e7ace226bba89cd7b7e0a4a4c1f118c7d565
John Doe <johndoe@example.com>
2023-12-01 10:10:39 +0100

    familyname commented

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -12,45 +12,47 @@ const familyMembersData = [
 ]
 function HomePage() {
   const { user, family } = useContext(AuthContext);
-  console.log("home page",user.family.familyName);
+  // console.log("home page", user.family.familyName);
   const [familyMember, setfamilyMember] = useState(familyMembersData)
   const [task, setTask] = useState(tasksData)
 
   return (
-  <>
-      { user.family || family? <div>
-     
-      <h1>welcome family:</h1>
-      <h3>{user.family.familyName}</h3>{/* import the family name from backend */}
-      <Link to='/createfamily'>create a Family</Link><br></br>
-      
-      <div className="family-member-container">
-      {familyMember.map((eachFamilyMember, index) => {
-        return (
-          < div className="family-member-card" key={eachFamilyMember._id} >
-            <img className="family-member-img" src={eachFamilyMember.img} alt={eachFamilyMember.name} />
-            <h2>{eachFamilyMember.name}</h2>
-          </div>)
-      })
-    }
-      </div>
-      <div className="task-container">{/* maybe we dont need this div if we keep the next one */}
-      {task.map((eachTask, index) => {
-        return ( <div className="task">{/* maybe we dont need this div if we keep the prev one */}
-          <p>{eachTask.taskDescription}</p>
-          <p>{eachTask.taskTime}</p>
-          <p>{eachTask.taskWeekDay}</p>
-          <p>{eachTask.taskAssignedTo}</p>
-          <p>{eachTask.taskComments}</p>
-        </div>)})}
-      </div>
-      <Link to="/createtask">
-        <button>New Task</button>
-      </Link>
-    </div >
-      : <Navigate to="/createfamily"/>
-      
-    }
+    <>
+      {user.family || family ?
+        <div>
+
+          <h1>welcome family:</h1>
+          {/* <h3>{user.family.familyName}</h3> import the family name from backend */}
+          <Link to='/createfamily'>create a Family</Link><br></br>
+
+          <div className="family-member-container">
+            {familyMember.map((eachFamilyMember, index) => {
+              return (
+                < div className="family-member-card" key={eachFamilyMember._id} >
+                  <img className="family-member-img" src={eachFamilyMember.img} alt={eachFamilyMember.name} />
+                  <h2>{eachFamilyMember.name}</h2>
+                </div>)
+            })
+            }
+          </div>
+          <div className="task-container">{/* maybe we dont need this div if we keep the next one */}
+            {task.map((eachTask, index) => {
+              return (<div className="task">{/* maybe we dont need this div if we keep the prev one */}
+                <p>{eachTask.taskDescription}</p>
+                <p>{eachTask.taskTime}</p>
+                <p>{eachTask.taskWeekDay}</p>
+                <p>{eachTask.taskAssignedTo}</p>
+                <p>{eachTask.taskComments}</p>
+              </div>)
+            })}
+          </div>
+          <Link to="/createtask">
+            <button>New Task</button>
+          </Link>
+        </div >
+        : <Navigate to="/createfamily" />
+
+      }
     </>
   );
 }
