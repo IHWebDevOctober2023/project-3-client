0bf8015490d596e8a0abd29cd42a857a632616a9
claratesmon <claraperezmontes@gmail.com>
2023-12-01 16:55:03 +0100

    post details display wip

--- src/pages/PostDetails/PostDetails.jsx
+++ src/pages/PostDetails/PostDetails.jsx
@@ -1,6 +1,26 @@
 import "./PostDetails.css";
+import { useEffect, useState } from "react";
+import { Navigate, redirect, useParams } from "react-router-dom";
+import { AuthContext } from "../../context/auth.context";
+import { useContext } from "react";
 
 function PostDetails() {
+    const { helpId } = useParams();
+    console.log("helpId",helpId)
+    const [helpData, setHelpData] = useState('')
+
+    useEffect(() => {
+        fetch(`http://localhost:5005/user/${helpId}`)
+        .then((response) => {
+            return response.json();
+        })
+        .then((jsonData) => {
+            setHelpData(jsonData);
+        })
+        .catch((err)=>console.log(err))
+
+    },[])
+
     return (
         <div>
             <h1>Post Details</h1>
