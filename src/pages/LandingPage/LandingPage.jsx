c379ad8cd946b87abf249f7734073f199ab0542e
Martin Palkov <martin.palkov@gmail.com>
2023-12-01 10:31:41 +0100

    added testimonies.map...

--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -1,24 +1,41 @@
-import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import Testimony from "../../components/Testimony/Testimony";
 import "./LandingPage.css";
+import { useContext, useEffect, useState } from "react";
 
 const LandingPage = () => {
-    const navigate = useNavigate();
+    const [testimoniesArray, setTestimoniesArray] = useState([]);
+    
+    useEffect(() => {
+        const BACKEND_ROOT = import.meta.env.VITE_SERVER_URL;
+        fetch(`${BACKEND_ROOT}/testimonies/landing`, {mode: 'cors'})
+            .then((response) => response.json())
+            .then ((responseJson) => {
+                setTestimoniesArray(responseJson);
+                // console.log("RESPONSE: ", testimoniesArray);
+            })     
+            .catch((err)=> console.log(err));
+    }, []);
 
     return (
         <div className="landing-wrapper">
             <section id="landing-upper">
                 <div id="landing-cta">
                     <h2>Do you need help? 3 helps for you are on us.</h2>
-                    <button onClick={ () => navigate("/signup")} className="cta-click">Get my free helps!</button>
+                    <Link to="/signup"><button className="cta-click">Get my free helps!</button></Link>
                 </div>
             </section>
         {/* In backend I need to populate creator and send it populated to frontend */}
             <section id="landing-testimonies">
-                <Testimony text="My Random Text" rating={5} creator={{name: "Alicia"}}/>
-                <Testimony text="Lorem Ipsum dolor sit amet lalalalalalala" rating={5} creator={{name: "Jose"}}/>
-                <Testimony text="My Random Text" rating={5} creator={{name: "Pepito"}}/>
-                <Testimony text="Lorem Ipsum dolor sit amet lalalalalalala" rating={2} creator={{name: "Mireia"}}/>
+                {
+                    testimoniesArray.map((eachTestimony, index) => {
+                        const {text, rating, creator} = eachTestimony;
+                        console.log("USER", creator)
+                        return (
+                            <Testimony key={index} text={text} rating={rating} creator={creator}/>
+                        )
+                    })
+                }
             </section>
         </div>
     );
