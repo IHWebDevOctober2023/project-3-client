531dad6494196643484861424f957153b2e6ae58
claratesmon <claraperezmontes@gmail.com>
2023-12-01 17:52:22 +0100

    logo, nav fix and post render

--- src/pages/PostDetails/PostDetails.jsx
+++ src/pages/PostDetails/PostDetails.jsx
@@ -5,12 +5,13 @@ import { AuthContext } from "../../context/auth.context";
 import { useContext } from "react";
 
 function PostDetails() {
+    const { isLoggedIn, user, logOutUser } = useContext(AuthContext);
     const { helpId } = useParams();
-    console.log("helpId",helpId)
     const [helpData, setHelpData] = useState('')
+    console.log("helpdata",helpData)
 
     useEffect(() => {
-        fetch(`http://localhost:5005/user/${helpId}`)
+        fetch(`http://localhost:5005/help-post/${helpId}`)
         .then((response) => {
             return response.json();
         })
@@ -24,6 +25,13 @@ function PostDetails() {
     return (
         <div>
             <h1>Post Details</h1>
+            <h3>{helpData.title}</h3>
+            <p>{helpData.location}</p>
+            <p>{helpData.description}</p>
+            <img  className="help-image" src={helpData.helpImageUrl} alt="" />
+            
+            <p>{user.name}</p>
+            <p>{helpData.catergory}</p>
         </div>
     );
 }
