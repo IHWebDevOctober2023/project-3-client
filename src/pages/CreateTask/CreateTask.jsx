2d2c5af1685e9a2e1a48fe1bfbcc1d56bf726915
Rafael Gerotto <rafael.gerotto@hotmail.com>
2023-12-01 16:09:52 +0100

    work on backend

--- src/pages/CreateTask/CreateTask.jsx
+++ src/pages/CreateTask/CreateTask.jsx
@@ -6,43 +6,59 @@ import { AuthContext } from "../../context/auth.context";
 
 
 function CreateTask() {
-    const [taskFamily, setTaskFamily] = useState("");
-    const [taskOwner, setTaskOwner] = useState("");
-    const [taskIcon, setTaskIcon] = useState("");
-    const [taskName, setTaskName] = useState("");
+    const [task , setTask] = useState([])
     const [taskDescription, setTaskDescription] = useState("");
     const [taskTime, setTaskTime] = useState("");
     const [taskWeekDay, setTaskWeekDay] = useState("");
+    /* const [taskFamily, setTaskFamily] = useState("");
+    const [taskOwner, setTaskOwner] = useState("");
+    const [taskIcon, setTaskIcon] = useState("");
+    const [taskName, setTaskName] = useState("");
     const [taskAssignedTo, setTaskAssignedTo] = useState("");
     const [taskIsDone, setTaskIsDone] = useState("");
     const [taskImgUploaded, setTaskImgUploaded] = useState("");
-    const [taskComments, setTaskComments] = useState("");
+    const [taskComments, setTaskComments] = useState(""); */
 
 
 
-    const handleTaskFamily = (e) => setTaskFamily(e.target.value);
-    const handleTaskOwner = (e) => setTaskOwner(e.target.value);
-    const handleTaskIcon = (e) => setTaskIcon(e.target.value);
-    const handleTaskName = (e) => setTaskName(e.target.value);
     const handleTaskDescription = (e) => setTaskDescription(e.target.value);
     const handleTaskTime = (e) => setTaskTime(e.target.value);
     const handleTaskWeekDay = (e) => setTaskWeekDay(e.target.value);
+    /* const handleTaskFamily = (e) => setTaskFamily(e.target.value);
+    const handleTaskOwner = (e) => setTaskOwner(e.target.value);
+    const handleTaskIcon = (e) => setTaskIcon(e.target.value);
+    const handleTaskName = (e) => setTaskName(e.target.value); */
 
     const navigate = useNavigate();
 
-    const handleSubmitTask = (e) => {///after you create a task you will redirect to HomePage
+    const handleSubmitTask = async (e) => {///after you create a task you will redirect to HomePage
         e.preventDefault()
-        navigate('/')
+        //console.log("working: ", taskDescription, taskTime, taskWeekDay);
+
+        try{
+            const submitTask = await fetch(`${import.meta.env.VITE_SERVER_URL}/family/task`, {
+                method: 'POST',
+                headers: {
+                    'Content-type': 'application/json'
+                },
+                body: JSON.stringify({taskDescription: taskDescription, taskTime: taskTime, taskWeekDay: taskWeekDay})
+            })
+            setTaskDescription("");
+            setTaskTime("");
+            setTaskWeekDay("");
+            navigate('/')
+        }
+        catch(error) {console.log(error)}
     }
     
     return (
         <>
-<form className="form-conteiner" onSubmit={handleSubmitTask}>
+<form className="form-container" onSubmit={handleSubmitTask}>
            {/*  falta and task icon, falta taskindone, falta assigned to, falta taskImgUploaded, falta task comments  */}
             <label>Description:</label>
             <textarea id="content" name="description" required onChange={handleTaskDescription}></textarea>
             <br/>
-            <label for="dayMoment"><b>When? Choose a timing interval to finish the task</b></label>
+            <label HtmlFor="dayMoment"><b>When? Choose a timing interval to finish the task</b></label>
             <select id="timing-interval" name="timingInterval" onChange={handleTaskTime}>
                 <option value="On Wake Up">On Wake Up</option>
                 <option value="Before Breakfast">Before Breakfast</option>
@@ -55,7 +71,7 @@ function CreateTask() {
                 <option value="Before Sleep">In the Afternoon</option>
             </select>
             <br/>
-            <label for="dayMoment"><b>Choose a day of the week</b></label>
+            <label htmlFor="dayMoment"><b>Choose a day of the week</b></label>
             <select id="day-moment" name="dayMoment" onChange={handleTaskWeekDay}>
                 <option value="Everyday">Everyday</option>
                 <option value="Monday">Monday</option>
@@ -67,9 +83,9 @@ function CreateTask() {
                 <option value="Sunday">Sunday</option>
             </select>
             <br/>
-            <Link to="/">
+
             <button type="submit">Create task</button>
-            </Link>
+
         </form>
         </>
     )
