b746217f0dc416511e234dfa13393427f08c6ceb
John Doe <johndoe@example.com>
2023-12-01 16:05:27 +0100

    displayTask

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,23 +1,28 @@
 import "./HomePage.css";
 import { useState } from 'react'
 import { Link, Navigate } from "react-router-dom";
-import { useContext } from "react";
+import { useContext , useEffect} from "react";
 import { AuthContext } from "../../context/auth.context";
-import tasksData from "./task.json"
 import Task from "../../components/Task/Task";
 import FamilyMember from "../../components/FamilyMember/FamilyMember";
 
-
-const familyMembersData = [
-  { _id: 1234, name: "Mario", img: "https://gravatar.com/avatar/865fdc42701e4d2f15599a6f1d34df6e?s=400&d=robohash&r=x" },
-  { _id: 2454, name: "Lisa", img: "https://gravatar.com/avatar/865fdc42701e4d2f15599a6f1d34df6e?s=400&d=robohash&r=x" }
-]
 function HomePage() {
   const { user, family } = useContext(AuthContext);
   // console.log("home page", user.family.familyName);
-  const [familyMember, setfamilyMember] = useState(familyMembersData)
-  const [task, setTask] = useState(tasksData)
+  const [task, setTask] = useState([])
   console.log(family);
+
+  const getTasks = async (event) => {
+    try {
+      const getTasksResponse = await fetch(`${import.meta.env.VITE_SERVER_URL}/family/task`)
+      const getTasks = await getTasksResponse.json()
+      console.log(getTasks);
+      setTask(getTasks)
+    } catch (error) { console.log(error);}
+  }
+  useEffect(()=>{
+    getTasks()
+  },[])
 
   return (
     <>
@@ -27,18 +32,6 @@ function HomePage() {
           <h1>welcome family:</h1>
           {/* <h3>{user.family.familyName}</h3> import the family name from backend */}
           <Link to='/createfamily'>create a Family</Link><br></br>
-
-          <div className="family-member-container">
-            {familyMember.map((eachFamilyMember, index) => {
-              return (
-                <FamilyMember
-                _id={eachFamilyMember._id}
-                img={eachFamilyMember.img}
-                name={eachFamilyMember.name}
-                />)
-            })
-            }
-          </div>
           <div className="task-container">{/* maybe we dont need this div if we keep the next one */}
             {task.map((eachTask, index) => {
               return (
