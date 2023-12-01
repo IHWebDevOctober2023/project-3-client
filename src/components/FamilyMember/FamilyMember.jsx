9146682d08ed283296a59660a5fdb7dac6832e3d
John Doe <johndoe@example.com>
2023-12-01 15:23:52 +0100

    displayFamilyMembers

--- src/components/FamilyMember/FamilyMember.jsx
+++ src/components/FamilyMember/FamilyMember.jsx
@@ -1,7 +1,7 @@
 import "./FamilyMember.css";
 
 function FamilyMember(props) {
-    return(<div className="family-member-card" key={props._id}>
+    return(<div className="family-member-card">
               <img className="family-member-img" src={props.img} alt={props.name} />
               <h2>{props.name}</h2>
   </div>)
--- src/pages/ProfilePage/ProfilePage.jsx
+++ src/pages/ProfilePage/ProfilePage.jsx
@@ -1,28 +1,46 @@
 import "./ProfilePage.css";
 import FamilyMember from "../../components/FamilyMember/FamilyMember";
 import { useState } from 'react'
+import { AuthContext } from "../../context/auth.context";
+import { useContext } from "react";
+import { useEffect } from "react";
 
 const familyMembersData = [
   { _id: 1234, name: "Mario", img: "https://gravatar.com/avatar/865fdc42701e4d2f15599a6f1d34df6e?s=400&d=robohash&r=x" },
   { _id: 2454, name: "Lisa", img: "https://gravatar.com/avatar/865fdc42701e4d2f15599a6f1d34df6e?s=400&d=robohash&r=x" }
 ]
 function ProfilePage() {
-  const [familyMember, setfamilyMember] = useState(familyMembersData)
+  const [familyMember, setfamilyMember] = useState([])
+  const { user, family } = useContext(AuthContext);
+
+  const getFamilyId = async (event) => {
+    try {
+      const familyMembersResponse = await fetch(`${import.meta.env.VITE_SERVER_URL}/family/familymembers/${family._id}`)
+      const familyMembers = await familyMembersResponse.json()
+      console.log(familyMembers);
+      setfamilyMember(familyMembers)
+    } catch (error) { console.log(error);}
+  }
+  useEffect(()=>{
+    getFamilyId()
+  },[])
 
   return (
     <div>
-      <h1>Profile page</h1>
+      {console.log(user, family)}
+      <h1>Hello {user.name.charAt(0).toUpperCase() + user.name.slice(1)}!</h1>
+      <h2>These are the members of the {family.familyName.charAt(0).toUpperCase() + family.familyName.slice(1)} family: </h2>
       <div className="family-member-container">
-            {familyMember.map((eachFamilyMember, index) => {
-              return (
-                <FamilyMember
-                _id={eachFamilyMember._id}
-                img={eachFamilyMember.img}
-                name={eachFamilyMember.name}
-                />)
-            })
-            }
-          </div>
+        {familyMember.map((eachFamilyMember, index) => {
+          return (
+            <FamilyMember
+              key={eachFamilyMember._id}
+              img={eachFamilyMember.userPicture}
+              name={eachFamilyMember.name}
+            />)
+        })
+        }
+      </div>
     </div>
   );
 }
