59c42c325301f3bd6a5d55b4bd3803ee3afdf347
claratesmon <claraperezmontes@gmail.com>
2023-12-04 14:57:25 +0100

    edit post

--- src/pages/EditHelpPage/EditHelpPage.jsx
+++ src/pages/EditHelpPage/EditHelpPage.jsx
@@ -1,5 +1,5 @@
 import "./EditHelpPage.css";
-import { useParams } from "react-router-dom";
+import { Link, Navigate, useParams } from "react-router-dom";
 import { useEffect, useState, useContext } from 'react'
 import { AuthContext } from "../../context/auth.context";
 
@@ -12,11 +12,11 @@ function EditHelpForm() {
     const [description, setDescription] = useState('')
     const [helpImageUrl, setHelpImage] = useState('')
     const [category, setCategory] = useState('')
-   
+
     const [isCompleted, setIsCompleted] = useState('')
 
     const { helpId } = useParams()
-   
+
 
     useEffect(() => {
 
@@ -27,13 +27,13 @@ function EditHelpForm() {
                 return response.json();
             })
             .then((responseJson) => {
-                const {location, helpImageUrl, title, description, category} = responseJson.foundHelpPost
+                const { location, helpImageUrl, title, description, category } = responseJson.foundHelpPost
                 setLocation(location)
                 setHelpImage(helpImageUrl)
                 setTitle(title)
                 setDescription(description)
                 setCategory(category)
-                console.log("este es el responseJson", responseJson.foundHelpPost)
+                //console.log("este es el responseJson", responseJson.foundHelpPost)
             })
             .catch((err) => console.log(err))
     }, [])
@@ -41,7 +41,7 @@ function EditHelpForm() {
 
     const putHelp = async (event) => {
         event.preventDefault();
-        const helpPut = {
+        const newHelpPut = {
             title,
             location,
             description,
@@ -49,21 +49,22 @@ function EditHelpForm() {
             category,
             isCompleted
         };
-        console.log(helpPut);
+        //console.log(helpPut);
 
         try {
             const BACKEND_ROOT = import.meta.env.VITE_SERVER_URL;
-            const response = await fetch(`${BACKEND_ROOT}/help-post/${helpId}`, {
-                method: "POST",
+            const response = await fetch(`${BACKEND_ROOT}/help-post/edithelp/${helpId}`, {
+                method: "PUT",
                 headers: {
                     "Content-Type": "application/json",
                 },
-                body: JSON.stringify(helpPut),
+                body: JSON.stringify(newHelpPut),
             }
                 , { mode: 'cors' });
-            const newHelpPut = await response.json();
-            setHelpPut((previousHelpPut) => [newHelpPut, ...previousHelpPut])
-            console.log("helpPut",helpPut);
+
+            const responseJson = await response.json();
+            setHelpPut(responseJson)
+            console.log("helpPutjson", responseJson);
 
         } catch (err) {
             console.log(err);
@@ -76,10 +77,11 @@ function EditHelpForm() {
         <div>
             <h1>Edit Help Request</h1>
 
-            
-            
+
+
             <div className="edit-help-container">
                 <form className="create-help-container" onSubmit={(event) => putHelp(event)}>
+
                     <label htmlFor="title">Title</label>
                     <textarea value={title} onChange={(event) => setTitle(event.target.value)} type="text" name="title" />
 
@@ -107,10 +109,10 @@ function EditHelpForm() {
                     </select>
 
 
-                    <p className="create-help-button" type="submit">SAVE CHANGES</p>
+                    <p onClick={(event) => putHelp(event)} className="create-help-button" type="submit">SAVE CHANGES</p>
                 </form>
             </div>
-         
+
 
         </div>
     );
